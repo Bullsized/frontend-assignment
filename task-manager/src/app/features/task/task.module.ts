c0c2f62286cef164ea9cb222dd68c20644d55d80
Ivan Stefanov <ivanstefanovbg@gmail.com>
2022-02-12 16:55:27 +0200

    adds font awesome to the project

--- task-manager/src/app/features/task/task.module.ts
+++ task-manager/src/app/features/task/task.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskComponent } from './task.component';
 import { TaskRoutingModule } from './task-routing.module';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskAddComponent } from './task-add/task-add.component';
+import { TaskEditComponent } from './task-edit/task-edit.component';
+import { TaskFormComponent } from './shared/task-form/task-form.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
   declarations: [
-    TaskComponent
+    TaskComponent,
+    TaskListComponent,
+    TaskAddComponent,
+    TaskEditComponent,
+    TaskFormComponent
   ],
   imports: [
     CommonModule,
-    TaskRoutingModule
+    TaskRoutingModule,
+    FontAwesomeModule
   ]
 })
-export class TaskModule { }
+export class TaskModule {
+}
