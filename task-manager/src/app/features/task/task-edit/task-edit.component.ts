5202562842d776f2f07242911965b907bfe23017
Ivan Stefanov <ivanstefanovbg@gmail.com>
2022-02-13 22:26:15 +0200

    task edit component shape up

--- task-manager/src/app/features/task/task-edit/task-edit.component.ts
+++ task-manager/src/app/features/task/task-edit/task-edit.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { faChevronCircleLeft } from '@fortawesome/free-solid-svg-icons';
+import { getTaskFormGroup } from '../shared/task-form-group/form.group';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-task-edit',
   templateUrl: './task-edit.component.html',
-  styleUrls: ['./task-edit.component.scss']
+  styleUrls: ['../task.component.scss']
 })
 export class TaskEditComponent implements OnInit {
+  readonly id: string = this.activatedRoute.snapshot.params.id;
 
-  constructor() { }
+  faChevronCircleLeft = faChevronCircleLeft;
+  form = getTaskFormGroup();
+
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
+  onEditSubmit() {
+  }
 }
