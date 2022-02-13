682a6b150736cdcf1e3380f3eaedc70b91d697a3
Ivan Stefanov <ivanstefanovbg@gmail.com>
2022-02-13 22:25:53 +0200

    task add component shape up

--- task-manager/src/app/features/task/task-add/task-add.component.ts
+++ task-manager/src/app/features/task/task-add/task-add.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { faChevronCircleLeft } from '@fortawesome/free-solid-svg-icons';
+import { getTaskFormGroup } from '../shared/task-form-group/form.group';
 
 @Component({
   selector: 'app-task-add',
   templateUrl: './task-add.component.html',
-  styleUrls: ['./task-add.component.scss']
+  styleUrls: ['../task.component.scss']
 })
 export class TaskAddComponent implements OnInit {
+  faChevronCircleLeft = faChevronCircleLeft;
+  form = getTaskFormGroup();
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  onAddSubmit() {
   }
 
 }
