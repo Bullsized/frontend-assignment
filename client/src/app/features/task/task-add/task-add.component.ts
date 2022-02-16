4cdf77e9831fa411a5d6dbc4d0e29560a83f3a44
Ivan Stefanov <ivanstefanovbg@gmail.com>
2022-02-16 23:22:17 +0200

    add task functionality

--- client/src/app/features/task/task-add/task-add.component.ts
+++ client/src/app/features/task/task-add/task-add.component.ts
@@ -1,23 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { faChevronCircleLeft } from '@fortawesome/free-solid-svg-icons';
 import { getTaskFormGroup } from '../shared/task-form-group/form.group';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { TasksService } from '../../../core/services/tasks.service';
+import { TasksAdapter } from '../../../core/adapters/tasks.adapter';
 
 @Component({
   selector: 'app-task-add',
   templateUrl: './task-add.component.html',
   styleUrls: ['../task.component.scss']
 })
-export class TaskAddComponent implements OnInit {
+export class TaskAddComponent {
   faChevronCircleLeft = faChevronCircleLeft;
   form = getTaskFormGroup();
 
-  constructor() {
+  constructor(
+    private tasksService: TasksService,
+    private tasksAdapter: TasksAdapter,
+    private router: Router
+  ) {
   }
 
-  ngOnInit(): void {
+  onAddSubmit(): void {
+    this.form.controls['title'].markAsTouched();
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    const taskRequestBody = this.tasksAdapter.formGroupToTaskRequest(this.form);
+
+    if (taskRequestBody) {
+      this.tasksService.createTask(taskRequestBody)
+        .subscribe(
+          () => this.onSuccess(),
+          (err) => this.onError(err)
+        );
+    }
   }
 
-  onAddSubmit() {
+  onSuccess() {
+    this.router.navigate(['/tasks']);
   }
 
+  onError(err: HttpErrorResponse) {
+    alert(`Whops, seems like there is no wind in the Sails...\nMaybe check the console?`)
+    console.error(err.message);
+  }
 }
