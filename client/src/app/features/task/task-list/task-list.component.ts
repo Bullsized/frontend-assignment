66b2698060ec63c3ff4b87963362727397be9be8
Ivan Stefanov <ivanstefanovbg@gmail.com>
2022-02-17 00:36:52 +0200

    task lists now rendered from API

--- client/src/app/features/task/task-list/task-list.component.ts
+++ client/src/app/features/task/task-list/task-list.component.ts
@@ -3,8 +3,12 @@ import {
   faPlusCircle,
   faCheckCircle,
   faTimesCircle,
-  faInfoCircle
+  faInfoCircle,
+  faDotCircle
 } from '@fortawesome/free-solid-svg-icons';
+import { TasksService } from '../../../core/services/tasks.service';
+import { Task } from '../../../core/interfaces/task';
+import { TasksAdapter } from '../../../core/adapters/tasks.adapter';
 
 @Component({
   selector: 'app-task-list',
@@ -16,11 +20,46 @@ export class TaskListComponent implements OnInit {
   faCheckCircle = faCheckCircle;
   faTimesCircle = faTimesCircle;
   faInfoCircle = faInfoCircle;
+  faDotCircle = faDotCircle;
 
-  constructor() {
+  tasks: Task[] = [];
+
+  constructor(
+    private tasksService: TasksService,
+    private tasksAdapter: TasksAdapter
+  ) {
   }
 
   ngOnInit(): void {
+    this.loadTasks();
   }
 
+  private loadTasks(): void {
+    this.tasksService.getTasks().subscribe(data => {
+      this.tasks = data;
+    })
+  }
+
+  deleteTask(task: Task) {
+    if (confirm('Are you sure you want to delete this task?') && task.id) {
+      this.tasksService.deleteTask(task.id).subscribe(
+        () => this.loadTasks(),
+        (err) => console.log(err)
+      );
+    }
+  }
+
+  completeTask(task: Task) {
+    const clonedTask = task;
+
+    clonedTask.isComplete = !task.isComplete;
+
+    const taskRequestBody = this.tasksAdapter.taskToTaskRequest(clonedTask);
+
+    if (taskRequestBody && task.id) {
+      this.tasksService.editTask(clonedTask, task.id).subscribe(
+        () => this.loadTasks(),
+        (err) => console.log(err))
+    }
+  }
 }
